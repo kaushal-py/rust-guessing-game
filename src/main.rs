762553cfe8fdfc677d5e0b8c7d35556be349eeae
kb-studios <2015kaushal.bhogale@ves.ac.in>
2018-09-17 21:21:00 +0530

    add random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::io;
+extern crate rand;
+use rand::Rng;
 
 fn main() {
 
-    let secret_number : u32 = 45;
+    let secret_number = rand::thread_rng().gen_range(1, 101);
     
     println!("Guess the number!");
 
@@ -19,7 +21,7 @@ fn main() {
             .expect("Please type a number!");
         
         if guess == secret_number {
-            println!("You guessed the number!! Congrats");
+            println!("You guessed the number!! The number was {}!", secret_number);
             break;
         }
         else if guess > secret_number{
