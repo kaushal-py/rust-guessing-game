a08e43b15b5b96accffa06cf9e1e16e3364f53ea
kb-studios <2015kaushal.bhogale@ves.ac.in>
2018-09-17 21:15:53 +0530

    working game with fixed secret number

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 use std::io;
 
 fn main() {
+
+    let secret_number : u32 = 45;
+    
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    loop{
+    
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        println!("Please input your guess: ");
+        
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+        
+        if guess == secret_number {
+            println!("You guessed the number!! Congrats");
+            break;
+        }
+        else if guess > secret_number{
+            println!("Guess a lower number.");
+        }
+        else if guess < secret_number{
+            println!("Guess a higher number");
+        }
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
+    }
 }
